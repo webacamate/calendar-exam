f9d1160edb6620fddd46614df51274edd65459d4
Andres <tatooyn@gmail.com>
2021-10-22 00:12:50 -0500

    github page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,6 +4,7 @@ export const Profile = () => {
             <h2>By Andrés Cardona Alvarez</h2> 
             <hr/>
             <h3>GitHub: <a href="https://github.com/webacamate/calendar-exam">https://github.com/webacamate/calendar-exam</a> </h3>
+            <h4>GitHub Page: <a href="https://webacamate.github.io/calendar-exam/">https://webacamate.github.io/calendar-exam/</a> </h4>
         </div>
     )
 }
